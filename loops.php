50a2ff06698de007bd800698f0a4312747fadae1
jayson7 <lexxiijoo70@gmail.com>
2024-08-26 23:48:48 +0100

    loops

--- loops.php
+++ loops.php
@@ -25,3 +25,30 @@ for($n = 0; $n < $round; $n++){
 
 
 ?>
+
+<?php
+
+// Creating an Associative Array
+$name_one = [
+	"Zack" => "Zara",
+	"Anthony" => "Any",
+	"Ram" => "Rani",
+	"Salim" => "Sara",
+	"Raghav" => "Ravina",
+];
+
+// Looping through an array using foreach
+echo "Looping using foreach: \n";
+foreach ($name_one as $val => $val_value) {
+	echo "Husband is " . $val . " and Wife is " . $val_value . "\n";
+}
+
+// Looping through an array using for
+echo "\nLooping using for: \n";
+$keys = array_keys($name_one);
+$round = count($name_one);
+
+for ($i = 0; $i < $round; ++$i) {
+	echo $keys[$i] . " " . $name_one[$keys[$i]] . "\n";
+}
+?>
