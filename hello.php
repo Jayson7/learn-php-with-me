fe7e09d8a591406ac1f5048562774155fbf9cad2
jayson7 <lexxiijoo70@gmail.com>
2024-08-26 16:01:45 +0100

    hello world

---
+++ hello.php
@@ -0,0 +1,24 @@
+<?php
+echo "Hello World";
+// first line of code
+
+?>
+
+
+<!-- second line of code  -->
+<?php
+/*
+this
+is 
+a 
+multi
+line
+comment
+
+*/
+
+echo "Hello Jayson";
+
+$newInfo ='Jayson is a beast who can code in Multiple Languages';
+echo $newInfo
+?>
---
+++ variables.php
@@ -0,0 +1,30 @@
+<?php
+// Here we can see that all echo 
+// statements are executed in the same manner
+ 
+$variable = 25;
+echo $variable;
+ECHO $variable;
+EcHo $variable;
+
+// but this line will show RUNTIME ERROR as
+// "Undefined Variable"
+echo $VARIABLE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
