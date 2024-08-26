43bd779dd878bffbabdc3f6ec54197c4acea7174
jayson7 <lexxiijoo70@gmail.com>
2024-08-26 19:54:01 +0100

    hello world

---
+++ conditions.php
@@ -0,0 +1,34 @@
+<?php 
+
+ global $x;
+ 
+ $x = false ;
+
+if ($x == true) {
+
+    $x = true;
+
+echo "x = ".$x;
+ } else {
+        $x = false;
+    
+echo "x = ".$x;   
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
