5090ec2388e1ca4ba6cf67211bff0e7c55f97624
jayson7 <lexxiijoo70@gmail.com>
2024-09-29 12:44:07 +0100

    learning laravel

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+route::get('/home', [HomeController::class, 'home']);
